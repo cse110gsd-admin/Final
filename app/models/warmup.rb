729263444d88d821c5779e02df7c5cd7988115fd
Jake Rabinowitz <jrabinow@ucsd.edu>
2011-06-04 15:55:39 -0700

    cleaning up (delete entries and warmups)

--- app/models/warmup.rb
+++
@@ -1,5 +0,0 @@
-class Warmup < ActiveRecord::Base
-  has_many   :exercises, :as => :exerciseable, :dependent => :destroy
-  accepts_nested_attributes_for :exercises
-  belongs_to :entry
-end
--- db/migrate/20110526085610_add_to_workouts.rb
+++
@@ -1,7 +0,0 @@
-class AddToWorkouts < ActiveRecord::Migration
-  def self.up
-  end
-
-  def self.down
-  end
-end
